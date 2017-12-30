2b55f9571dc4211b00f7cdc7d2eaa0bcc9340579
Kamil Stangret <kamilstangret@wp.pl>
2017-12-30 23:23:50 +0100

    Add index.html and js file with contents

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var buttonItemsClass = document.getElementsByClassName('button');
+
+console.log(buttonItemsClass);
+
+var numberOfButtons = buttonItemsClass.length
+for (i = 0 ; i <= numberOfButtons ; i++) {
+  console.log(buttonItemsClass.);
+  
+}
+console.log(buttonItemsClass.innerText);
